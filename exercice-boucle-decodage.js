0cc2ed7b36bf5fc8a22db0d3668c7ffe7912f77e
teo-crth <teoconrath@gmail.com>
2024-11-28 09:54:34 +0100

    exo boucle

---
+++ exercice-boucle-decodage.js
@@ -0,0 +1,10 @@
+// Use a loop to remove the 'X' and use console.log to reveal the message 
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ", "X", "!", "X"];
+
+for (let i = 0; i < hiddenMessage.length; i++) {
+  if (hiddenMessage[i] === "X") {
+    hiddenMessage[i] = "";
+  }
+}
+
+console.log(hiddenMessage.join(""));
