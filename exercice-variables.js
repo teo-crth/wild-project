8a1d74c48b44561d1a1cfcbc2165548dbcc1c65e
teo-crth <teoconrath@gmail.com>
2024-11-28 09:51:34 +0100

    exo variables

---
+++ exercice-variables.js
@@ -0,0 +1,20 @@
+🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+variable keyword you have learned in the quest.
+ex: const name = "David";
+
+*/
+
+const name = "David"; // ✅ Good!  
+const lastName = "Bruno"; // ❌ the keyword to declare the variable is missing 
+
+let davidPresentation = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; // ❌ the variable name is not explicit. (this is David's biography)
+
+const davidPicture = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+
+const cityName = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+
+let followers = 109; // ❌ the keyword to declare the variable is missing 
+let following = 200; // ❌ the keyword to declare the variable is missing 
+
+// 🏁 Exercise 02 - David is following one more account increment the total of following account
+following = following++;
